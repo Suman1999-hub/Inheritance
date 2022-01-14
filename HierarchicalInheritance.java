6acf6e3e12776b8a1c442e9696fda4706e696609
SUMAN RANA <68540296+Suman1999-hub@users.noreply.github.com>
2022-01-15 01:55:17 +0530

    Add files via upload

---
+++ HierarchicalInheritance.java
@@ -0,0 +1,46 @@
+package OOPs;
+class Fruit{
+    public void fruit(){
+        System.out.println("Fruit is: ");
+    }
+}
+class Mango extends Fruit{
+    public void mango(){
+        System.out.println("Mango"+",");
+    }
+}
+class Banana extends Fruit{
+    public void banana(){
+        System.out.println("Banana"+",");
+    }
+}
+class Orange extends Fruit{
+
+    public void orange(){
+        System.out.println("Orange"+",");
+    }
+}
+class Apple extends Fruit{
+    public void apple(){
+        System.out.print("Apple");
+    }
+}
+public class HierarchicalInheritance {
+    public static void main(String[] args) {
+        Mango A=new Mango();
+        A.fruit();
+        A.mango();
+        Banana B=new Banana();
+        B.fruit();
+        B.banana();
+        Orange C=new Orange();
+        C.fruit();
+        C.orange();
+        Apple D=new Apple();
+        D.fruit();
+        D.apple();
+
+
+
+    }
+}
